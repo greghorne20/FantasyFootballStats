77f6fafab88aa718e44fa86c3c04810b6fc50c75
greg <greg@greg-laptop>
2015-09-18 15:51:12 -0400

    All info is fetched.  Rankings are calculated, incl power rankings.  works on any public espn league

--- src/fantasyfootballstats/FantasyFootballStats.java
+++ src/fantasyfootballstats/FantasyFootballStats.java
@@ -16,6 +16,13 @@ import org.jsoup.select.*;
 
 import java.io.IOException;
 import java.lang.Double;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Scanner;
+import org.jsoup.Connection.Method;
 
 /**
  *
@@ -23,8 +30,9 @@ import java.lang.Double;
  */
 public class FantasyFootballStats {
 
-    final static int NUMOFTEAMS = 12;
-    final static int NUMOFWEEKS = 13;
+    static int NUM_OF_TEAMS;
+    static String LEAGUE_NAME;
+    static int NUM_OF_WEEKS;
     final static int ROWS_BTWN_WEEKS = 9;
     
     
@@ -34,21 +42,94 @@ public class FantasyFootballStats {
      */
     public static void main(String[] args) throws IOException
     {
+        /*
+        // NOT WORKING
+        //With this you login and a session is created
+        Connection.Response res = Jsoup.connect("https://cdn.registerdisney.go.com/v2/responder/responder.js")
+        .data("text", "greghorne20+espn@gmail.com", "password", "bday1390")
+        .method(Method.POST)
+        .execute();
+        
+
+        //This will get you cookies
+        Map<String, String> loginCookies = res.cookies();
+        */
+        System.out.println("Please enter the league ID (i.e. the number found at the end of league URL");
+        System.out.print("League ID: ");
+        Scanner scanner = new Scanner(System.in);
+        String leagueID = scanner.nextLine();
+        //String leagueID = "69673";
+        
+        String settingsUrl = "http://games.espn.go.com/ffl/leaguesetup/settings?leagueId=" + leagueID;
+        Document doc = Jsoup.connect(settingsUrl).get();
+        Element table = doc.select("table").get(1);
+        Elements rows = table.select("tr");
+        
+        Elements row = doc.select("tr:contains(Number Of Teams)");
+        NUM_OF_TEAMS = Integer.parseInt( row.get(1).text().substring(16));
+        
+        row = doc.select("tr:contains(League Name)");
+        LEAGUE_NAME = ( row.get(1).text().substring(12));
+        
+        row = doc.select("tr:contains(Regular Season Matchups)");
+        NUM_OF_WEEKS = Integer.parseInt( row.get(1).text().substring(24,26));
+        
+        System.out.println(LEAGUE_NAME);
+        System.out.println("TEAMS: " + NUM_OF_TEAMS);
+        System.out.println("WEEKS IN REG SEASON: " + NUM_OF_WEEKS);
+        System.out.println("======================================\n");
         
         
-        String url = "http://games.espn.go.com/ffl/schedule?leagueId=69673";
+        /*
+        //THIS WORKED FOR OUR LEAGUE BUT IS NOT UNIVERSAL
+        Element row = rows.get(2);
+        Elements cols = row.select("td");
+        NUM_OF_TEAMS = Integer.parseInt( cols.get(1).text());
+        
+        row = rows.get(1);
+        cols = row.select("td");
+        LEAGUE_NAME =  cols.get(1).text();
+        
+        table = doc.select("table").get(17);
+        rows = table.select("tr");
+        row = rows.get(3);
+        cols = row.select("td");
+        NUM_OF_WEEKS =  Integer.parseInt( cols.get(1).text().split(" ")[0]);
+        */
+        
+        
+        
+        String url = "http://games.espn.go.com/ffl/schedule?leagueId=" + leagueID;    
           
-        Document doc = Jsoup.connect(url).get();
+        doc = Jsoup.connect(url).get();
         
-        Team[] teams = new Team[NUMOFTEAMS];
+        Team[] teams = new Team[NUM_OF_TEAMS];
         
         fetchTeamNames(teams, doc);
         getSeasonInfo(teams, doc);
         
 
         
-        System.out.println(teams[1].getName());
-        System.out.println(teams[1].getScoresByWeek(1));
+        for(Team team: teams)
+        {
+            System.out.println("Name: " + team.getName());
+            System.out.println("Record: " + team.getWins() + "-" + team.getLosses() + "-" + team.getTies());
+            System.out.println("Rank: " + team.getRank());
+            System.out.println("Points For: " + team.getPointsFor());
+            System.out.println("Points Rank: " + team.getPointsRank());
+            System.out.println("Breakdown Record: " + team.getBreakdownWins() + "-" + team.getBreakdownLosses() + "-" + team.getBreakdownTies());
+            System.out.println("Breakdown Rank: " + team.getBreakdownRank());
+            System.out.println("----------------------------");
+        }
+        
+        System.out.println("\nPower Rankings");
+        System.out.printf("%-25s%-15s%-15s%-15s%-15s", new Object[]{ "Team" , "PointsFor" , "Record" , "Brkdwn. Rec." , "Power"} );
+        System.out.println();
+        for(Team team: teams)
+        {
+            System.out.printf("%-25s%-15d%-15d%-15d%-15d",  team.getName() , team.getPointsRank() , team.getRank() , team.getBreakdownRank() , team.getPowerRanking());
+            System.out.println();
+        }
         
     
            
@@ -59,14 +140,14 @@ public class FantasyFootballStats {
     }
     
     /*
-    * Takes a document of an ESPN fantasy Schedule page, creates NUMOFTEAMS Teams
+    * Takes a document of an ESPN fantasy Schedule page, creates NUM_OF_TEAMS Teams
     * and sets their team names
     */
     public static void fetchTeamNames(Team[] teams, Document doc)
     {
                 // for loop that iterators through the rows of teams and sets their names
         int t = 0;  // additional iterator to go through teams since there are two teams in each row
-        for(int i=2; i < 2 + (NUMOFTEAMS / 2); i++)
+        for(int i=2; i < 2 + (NUM_OF_TEAMS / 2); i++)
         {
             Element table = doc.select("table").get(1);
             Elements rows = table.select("tr");
@@ -92,18 +173,23 @@ public class FantasyFootballStats {
         Elements rows = table.select("tr");
         
         int i = 2; //where first tr starts with teams
-        for(int week = 1; i < NUMOFWEEKS; week++ )
+        for(int week = 1; i < NUM_OF_WEEKS; week++ )
         {
             Element row = rows.get(i);
             Elements cols = row.select("td");
             String test = cols.get(5).text();
-            if ( !(cols.get(5).text().equals("Preview")) )
-                getWeekInfo(teams, doc, week);
+            if ( (cols.get(5).text().equals("Preview")) || ( (cols.get(5).text().equals("Box")))  )
+                break; // no more weeks have been played
             else
-                return; // no more weeks have been played.
+                getWeekInfo(teams, doc, week);
                 
             i = i + ROWS_BTWN_WEEKS;
         }
+        
+        rankTeamsByBreakdownRecord(teams);
+        rankTeamsByPointsFor(teams);
+        rankTeamsByWinPerct(teams);
+        setPowerRankings(teams);
         
     }
     
@@ -116,15 +202,21 @@ public class FantasyFootballStats {
             
         int startingRow = 2 + (week-1) * ROWS_BTWN_WEEKS;
         
-        for(int i = startingRow; i < startingRow + (NUMOFTEAMS / 2); i++)
+        double[] weeklyPoints = new double[NUM_OF_TEAMS];
+        int weeklyPointsItr = 0;
+        
+        for(int i = startingRow; i < startingRow + (NUM_OF_TEAMS / 2); i++)
         {
             Element row = rows.get(i);
             Elements cols = row.select("td");
+
             
+            Double awayScore = Double.parseDouble(cols.get(5).text().split("\\-")[0]);                
+            Double homeScore = Double.parseDouble(cols.get(5).text().split("\\-")[1]);                
+                
+                     
             for( Team team: teams)
             {
-                Double awayScore = Double.parseDouble(cols.get(5).text().split("\\-")[0]);                
-                Double homeScore = Double.parseDouble(cols.get(5).text().split("\\-")[1]);                
                 
                 if(cols.get(0).text().split("\\(")[0].equals(team.getName())) // if they are away team
                 {
@@ -137,6 +229,9 @@ public class FantasyFootballStats {
                         team.setTies( team.getTies() + 1); 
                     
                     team.setPointsFor(awayScore + team.getPointsFor());
+                    
+                    weeklyPoints[weeklyPointsItr] = awayScore;
+                    weeklyPointsItr++;
                 }
                 else if(cols.get(3).text().split("\\(")[0].equals(team.getName()))  // if they are home team
                 {
@@ -151,11 +246,114 @@ public class FantasyFootballStats {
                         team.setTies( team.getTies() + 1); 
                     
                     team.setPointsFor(homeScore + team.getPointsFor());
+                    
+                    weeklyPoints[weeklyPointsItr] = homeScore;
+                    weeklyPointsItr++;
                 }
             }
-
-
-
+        }
+        
+        
+        updateBreakdownRecord(teams, weeklyPoints, week);
+    }
+    
+    public static void updateBreakdownRecord(Team[] teams, double[] weeklyPoints, int week)
+    {
+        Arrays.sort(weeklyPoints);
+        for(Team team: teams)
+        {
+            // Check teams score against each score that week
+            for(Double score: weeklyPoints)
+            {
+                if(team.getScoresByWeek(week) > score)
+                    team.setBreakdownWins( team.getBreakdownWins() + 1);
+                else if(team.getScoresByWeek(week) < score)
+                    team.setBreakdownLosses( team.getBreakdownLosses() + 1);
+                else
+                    team.setBreakdownTies( team.getBreakdownTies() + 1);
+            }
+            
+            // The team tied itself, but that shouldn't count, so
+            team.setBreakdownTies( team.getBreakdownTies() - 1);
+            
+        }
+    }
+    
+    public static void rankTeamsByBreakdownRecord(Team[] teams)
+    {
+        
+        ArrayList<Team> teamsRankedByBreakdown = new ArrayList<>(Arrays.asList(teams));
+        Collections.sort(teamsRankedByBreakdown, Team.BreakdownComparator);
+        
+        
+        for(int i = 0; i < teamsRankedByBreakdown.size(); i++)
+        {
+            for(Team team: teams)
+            {
+                if(team.getName().equals( teamsRankedByBreakdown.get(i).getName()))
+                {
+                    team.setBreakdownRank(i+1);
+                    break;
+                }
+            }
+        }
+        
+    }
+    
+    public static void rankTeamsByPointsFor(Team[] teams)
+    {
+        ArrayList<Team> teamsRankedByPF = new ArrayList<>(Arrays.asList(teams));
+        Collections.sort(teamsRankedByPF, Team.PointsForComparator);
+        
+        
+        for(int i = 0; i < teamsRankedByPF.size(); i++)
+        {
+            for(Team team: teams)
+            {
+                if(team.getName().equals( teamsRankedByPF.get(i).getName()))
+                {
+                    team.setPointsRank(i+1);
+                    break;
+                }
+            }
+        }
+    }
+    
+    public static void rankTeamsByWinPerct(Team[] teams)
+    {
+        ArrayList<Team> teamsRankedByWP = new ArrayList<>(Arrays.asList(teams));
+        Collections.sort(teamsRankedByWP);
+        
+        
+        for(int i = 0; i < teamsRankedByWP.size(); i++)
+        {
+            for(Team team: teams)
+            {
+                if(team.getName().equals( teamsRankedByWP.get(i).getName()))
+                {
+                    team.setRank(i+1);
+                    break;
+                }
+            }
+        }
+    }
+    
+    public static void setPowerRankings(Team[] teams)
+    {
+        ArrayList<Team> teamsRankedByPR = new ArrayList<>(Arrays.asList(teams));
+        Collections.sort(teamsRankedByPR, Team.PowerRankingsComparator);
+        
+        
+        for(int i = 0; i < teamsRankedByPR.size(); i++)
+        {
+            for(Team team: teams)
+            {
+                if(team.getName().equals( teamsRankedByPR.get(i).getName()))
+                {
+                    team.setPowerRank(i+1);
+                    break;
+                }
+            }
         }
     }
     
--- src/fantasyfootballstats/Team.java
+++ src/fantasyfootballstats/Team.java
@@ -5,11 +5,13 @@
  */
 package fantasyfootballstats;
 
+import java.util.Comparator;
+
 /**
  *
  * @author Greg Horne
  */
-public class Team
+public class Team implements Comparator<Team>, Comparable<Team>
 {
     public final int NUMOFWEEKS = 16;
     
@@ -17,8 +19,20 @@ public class Team
     int wins = 0;
     int losses = 0;
     int ties = 0;
+    int rank;
+            
     Double pointsFor = 0.0;
+    int pointsRank;
+    
     Double[] scoresByWeek = new Double[NUMOFWEEKS];
+    
+    // variables for breakdown record
+    int breakdownWins = 0;
+    int breakdownLosses = 0;
+    int breakdownTies = 0;
+    int breakdownRank;
+    
+    int powerRanking;
     
     
     Team()
@@ -58,6 +72,41 @@ public class Team
         return scoresByWeek[week];
     }
     
+    public int getBreakdownWins()
+    {
+        return breakdownWins;
+    }
+    
+    public int getBreakdownLosses()
+    {
+        return breakdownLosses;
+    }
+    
+    public int getBreakdownTies()
+    {
+        return breakdownTies;
+    }
+    
+    public int getBreakdownRank()
+    {
+        return breakdownRank;
+    }
+    
+    public int getPointsRank()
+    {
+        return pointsRank;
+    }
+    
+    public int getRank()
+    {
+        return rank;
+    }
+    
+    public int getPowerRanking()
+    {
+        return powerRanking;
+    }
+    
     
     //Mutator Methods
     
@@ -90,4 +139,128 @@ public class Team
     {
         scoresByWeek[week] = score;
     }
+    
+    public void setBreakdownWins(int wins)
+    {
+        breakdownWins = wins;
+    }
+    
+    public void setBreakdownLosses(int losses)
+    {
+        breakdownLosses = losses;
+    }
+    
+    public void setBreakdownTies(int ties)
+    {
+        breakdownTies = ties;
+    }
+    
+    public void setBreakdownRank(int rank)
+    {
+        breakdownRank = rank;
+    }
+    
+    public void setPointsRank(int rank)
+    {
+        pointsRank = rank;
+    }
+    
+    public void setRank(int ranking)
+    {
+        rank = ranking;
+    }
+    
+    public void setPowerRank(int rank)
+    {
+        powerRanking = rank;
+    }
+    
+    //Override comparator methods.
+    //Teams are compared first by winning percentage, then by Points For
+    @Override
+    public int compareTo(Team otherTeam)
+    {
+        return compare(this, otherTeam);
+    }
+    
+    
+    @Override
+    public int compare(Team teamA, Team teamB)
+    {
+        //If teams have the same winning percentage, compare by PointsFor
+        if ((teamA.wins + teamA.ties / 2)  == (teamB.wins + teamB.ties / 2))
+        {
+            if(teamA.pointsFor > teamB.pointsFor)
+                return -1;
+            else
+                return 1;
+        }
+        
+        else
+        {
+            if ((teamA.wins + teamA.ties / 2)  > (teamB.wins + teamB.ties / 2))
+                return -1;
+            else
+                return 1;
+        }
+ 
+    }
+    
+    
+    public static Comparator<Team> BreakdownComparator = new Comparator<Team>()
+    {
+
+        @Override
+        public int compare(Team teamA, Team teamB)
+        {
+            //If teams have the same winning percentage, compare by PointsFor
+            if ((teamA.breakdownWins + teamA.breakdownTies / 2)  == (teamB.breakdownWins + teamB.breakdownTies / 2))
+            {
+                if(teamA.pointsFor > teamB.pointsFor)
+                    return -1;
+                else
+                    return 1;
+            }
+
+            else
+            {
+                if ((teamA.breakdownWins + teamA.breakdownTies / 2)  > (teamB.breakdownWins + teamB.breakdownTies / 2))
+                    return -1;
+                else
+                    return 1;
+            }
+
+        }
+    };
+    
+    public static Comparator<Team> PointsForComparator = new Comparator<Team>()
+    {
+
+        @Override
+        public int compare(Team teamA, Team teamB)
+        {
+
+            if(teamA.pointsFor > teamB.pointsFor)
+                return -1;
+            else
+                return 1;
+
+        }
+    };
+    
+    public static Comparator<Team> PowerRankingsComparator = new Comparator<Team>()
+    {
+
+        @Override
+        public int compare(Team teamA, Team teamB)
+        {
+
+            if(teamA.getRank() + teamA.getBreakdownRank() + teamA.getPointsRank()
+                    < teamB.getRank() + teamB.getBreakdownRank() + teamB.getPointsRank())
+                return -1;
+            else
+                return 1;
+
+        }
+    };
 }
